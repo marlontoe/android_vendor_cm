7f816c6f77107aaa7d678a92d24b57078f5c0ef0
Vaibhav Pandey <contact@vaibhavpandey.com>
2016-03-12 13:31:46 +0100

    P6 cm11 patches
    Change-Id: I04a76603630e1856da48e274c80a2af5a901b51d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
+for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
 do
     add_lunch_combo $combo
+    add_lunch_combo cm_hwp6_u06-userdebug
+    add_lunch_combo full_hwp6_u06-userdebug
 done
